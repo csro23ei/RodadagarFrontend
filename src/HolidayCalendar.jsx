a94c34b98316b27323e6b556bee6f6c3d7461105
csro23ei <dante030724@gmail.com>
2024-06-16 12:21:57 +0200

    Kopia av mit project 2

---
+++ src/HolidayCalendar.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import Calendar from 'react-calendar';
+import axios from 'axios';
+import 'react-calendar/dist/Calendar.css';
+import './HolidayCalendar.css';
+
+const HolidayCalendar = ({ onDateClick }) => {
+    const [holidays, setHolidays] = useState([]);
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        axios.get('http://sholiday.faboul.se/dagar/v2.1/2015')  //<--- Url här!!
+            .then(response => {
+                console.log(response.data);
+                setHolidays(Array.isArray(response.data) ? response.data : []);
+            })
+            .catch(error => {
+                console.error('Error fetching holidays: ', error);
+                setHolidays([]);  
+            });
+    }, []);
+
+    const tileContent = ({ date, view }) => {
+        if (view === 'month') {
+            if (Array.isArray(holidays)) {
+                const holiday = holidays.find(h => new Date(h.date).toDateString() === date.toDateString());
+                return holiday ? <div className="holiday">{holiday.name}</div> : null;
+            }
+            return null;
+        }
+    };
+
+    const handleDateClick = (value) => {
+        onDateClick(value);
+        setDate(value);
+    };
+
+    return (
+        <div>
+            <Calendar
+                onClickDay={handleDateClick}
+                value={date}
+                tileContent={tileContent}
+            />
+        </div>
+    );
+};
+
+export default HolidayCalendar;
